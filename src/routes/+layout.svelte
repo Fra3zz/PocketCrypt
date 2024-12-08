1e530be3fd880dc6238f0fc5736676ba53451dc7
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-12-08 09:42:59 -0600

    Add: Default CSS and link to keys page.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	let { children } = $props();
+</script>
+
+
+<main>
+    <div>
+        <div>
+            <a href="/keys">Make Keys</a>
+        </div>
+    </div>
+</main>
+
+<style>
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+
+  color: #b5b2b2;
+  background-color: #585151;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+</style>
+
+
+{@render children()}
