9f497cf175452f3fadac66988b012fe637894861
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-12-08 12:49:00 -0600

    Update: Uses rust backend to generate keys.

--- src/routes/keys/+page.svelte
+++ src/routes/keys/+page.svelte
@@ -1,33 +1,65 @@
 <script lang="ts">
-import { generateKeysPEM } from "./functions";
+  import { invoke } from "@tauri-apps/api/core";
+  
+  let privateKey: string;
+  let publicKey: string;
+  let keySize: number = 2048; // Default key size
+  let loading: boolean = false; // Loading state
 
-const KEYS = generateKeysPEM(2048)
+  const makeKeys = async (strKeySize: number) => {
+    loading = true; // Set loading state to true
 
+    let keySize = Number(strKeySize);
+
+    try {
+      // Specify the expected return type of the invoke function
+      const [privKey, pubKey]: [string, string] = await invoke<[string, string]>('make_rsa_keys', { keySize });
+      privateKey = privKey;
+      publicKey = pubKey;
+    } catch (error) {
+      console.error("Error generating keys:", error);
+    } finally {
+      loading = false; // Set loading state to false after keys are generated
+    }
+  };
+
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
 </script>
 
 <style>
-    .container {
-        display: flex;
-        align-items: baseline;
-        margin: 5px;
-        border: black solid;
-        padding: 2px;
-        
-    }
+  /* Add your styles here */
 </style>
 
+<form on:submit|preventDefault={() => makeKeys(keySize)}>
+  <select name="key size" id="Key Size" bind:value={keySize}>
+    <option value="1024">1024</option>
+    <option value="2048">2048</option>
+    <option value="3072">3072</option>
+    <option value="4096">4096</option>
+  </select>
+  <button type="submit">Generate Keys</button>
+</form>
 
-<div class="container">
-    <div class="container">
-        <label for="privKey" style="text-align: center;">Private Key</label>
-        <pre id="privKey">
-            {KEYS.privateKey}
-        </pre>
+{#if loading}
+  <h1>LOADING KEYS</h1>
+{:else if privateKey && publicKey}
+  <div>
+    <div>
+      <label for="privKey">Private Key</label>
+      <pre id="privKey">{privateKey}</pre>
+      <button on:click={() => copyToClipboard(privateKey)}>Copy Private Key</button>
     </div>
-    <div class="container">
-        <label for="pubKey">Public Key</label>
-        <pre id="pubKey">
-            {KEYS.publicKey}
-        </pre>
+    <div class="key-container">
+      <label for="pubKey">Public Key</label>
+      <pre id="pubKey">{publicKey}</pre>
+      <button on:click={() => copyToClipboard(publicKey)}>Copy Public Key</button>
     </div>
-</div>
+  </div>
+{/if}
