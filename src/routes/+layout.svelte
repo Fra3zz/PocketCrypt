4fe3041f67c16352d24edc10c6c6175f1fffe8ac
Fra3zz <112791937+Fra3zz@users.noreply.github.com>
2024-12-08 19:03:09 -0600

    Add: PKCS#12 file creation using cert, key, and CA cert chain.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,6 +28,9 @@
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="/sign_csr">Sign CSR</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/make_pkcs">Make PKCS#12</a>
+          </li>
         </ul>
       </div>
     </div>
